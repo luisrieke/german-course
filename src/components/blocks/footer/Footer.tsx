493ccb12c7038eaa2fd880f9d4d07b6f847e4381
luis-rieke <luis.rieke@europace.de>
2022-11-08 20:24:20 +0100

    Update Footer

--- src/components/blocks/footer/Footer.tsx
+++ src/components/blocks/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { Contact } from '../contact';
-import SocialLinks from 'components/reuseable/SocialLinks';
+import NextLink from 'components/reuseable/links/NextLink';
 
 const Footer: FC = () => {
   return (
@@ -14,45 +14,10 @@ const Footer: FC = () => {
       <div className="container pb-12 text-center">
         <div className="row mt-n10 mt-lg-0">
           <div className="col-xl-10 mx-auto">
-            <div className="row mb-3">
-              <div className="col-md-4">
-                <div className="widget">
-                  <h4 className="widget-title">Address</h4>
-                  <address>
-                    Moonshine St. 14/05 <br className="d-none d-md-block" /> Light City, London, UK
-                  </address>
-                </div>
-              </div>
-
-              <div className="col-md-4">
-                <div className="widget">
-                  <h4 className="widget-title">Phone</h4>
-                  <p>
-                    00 (123) 456 78 90 <br />
-                    00 (987) 654 32 10
-                  </p>
-                </div>
-              </div>
-
-              <div className="col-md-4">
-                <div className="widget">
-                  <h4 className="widget-title">E-mail</h4>
-
-                  <a href="mailto:sandbox@email.com" className="link-body">
-                    sandbox@email.com
-                  </a>
-                  <br className="d-none d-md-block" />
-
-                  <a href="mailto:help@sandbox.com" className="link-body">
-                    help@sandbox.com
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <p>© 2022 Sandbox. All rights reserved.</p>
-
-            <SocialLinks className="nav social justify-content-center" />
+            <p>
+              © Spacifik UG (haftungsbeschränkt). All rights reserved.{' '}
+              <NextLink title="Imprint & Data privacy" href="/terms" />
+            </p>
           </div>
         </div>
       </div>
