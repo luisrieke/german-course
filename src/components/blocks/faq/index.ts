81aaed31b940afc9ba8ee1b63a34641830dc86f1
luis-rieke <luis.rieke@europace.de>
2022-11-08 18:46:21 +0100

    project setup

---
+++ src/components/blocks/faq/index.ts
@@ -0,0 +1,8 @@
+import FAQ1 from './FAQ1';
+import FAQ2 from './FAQ2';
+import FAQ3 from './FAQ3';
+import FAQ4 from './FAQ4';
+import FAQ5 from './FAQ5';
+import FAQ6 from './FAQ6';
+
+export { FAQ1, FAQ2, FAQ3, FAQ4, FAQ5, FAQ6 };
