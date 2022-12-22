55561a0aa4b9acc383626b516673c06a034ffff8
luisrieke <luis.rieke@spacifik.com>
2022-12-22 18:06:55 +0100

    try padding

--- src/components/cookies.tsx
+++ src/components/cookies.tsx
@@ -8,6 +8,8 @@ const Cookies = () => {
         src="https://consent.cookiebot.com/1315d47d-d606-4f96-a103-c9afa6469781/cd.js"
         type="text/javascript"
         async
+        padding-left="200px"
+        padding-right="200px"
       ></Script>
     </>
   );
